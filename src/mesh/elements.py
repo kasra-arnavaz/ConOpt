fd76f13beb773e59e93541b8f09c1fe3b394fa64
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-07-26 21:29:49 +0200

    added mesh/elements and its tests

---
+++ src/mesh/elements.py
@@ -0,0 +1,52 @@
+import torch
+from attrs import define, field
+
+@define
+class Elements:
+    triangles: torch.Tensor = field(default=None)
+    tetrahedra: torch.Tensor = field(default=None)
+
+    @property
+    def num_triangles(self):
+        return self.triangles.shape[0] if self.triangles is not None else 0
+
+    @property
+    def num_tetrahedra(self):
+        return self.tetrahedra.shape[0] if self.tetrahedra is not None else 0
+
+    @triangles.validator
+    def _validate_triangles(self, attribute, value):
+        if value is not None:
+            self._validate_common_traits_of_triangles_and_tetrahedra(name=attribute.name, value=value)
+            self._validate_triangles_shape(name=attribute.name, value=value)
+
+    @tetrahedra.validator
+    def _validate_tetrahedra(self, attribute, value):
+        if value is not None:
+            self._validate_common_traits_of_triangles_and_tetrahedra(name=attribute.name, value=value)
+            self._validate_tetrahedra_shape(name=attribute.name, value=value)
+
+    def _validate_common_traits_of_triangles_and_tetrahedra(self, name, value):
+        self._validate_type(name=name, value=value)
+        self._validate_2d_tensor(name=name, value=value)
+        self._validate_dtype(name=name, value=value)
+
+    def _validate_type(self, name, value):
+        if not isinstance(value, torch.Tensor):
+            raise TypeError(f"Expected <{name}> to be of type torch.Tensor, got {type(value)}.")
+
+    def _validate_2d_tensor(self, name, value):
+        if len(value.shape) != 2:
+            raise ValueError(f"Expected <{name}> to be a 2D tensor, got {len(value.shape)}D tensor.")
+
+    def _validate_dtype(self, name, value):
+        if value.dtype != torch.int64:
+            raise ValueError(f"Expected dtype of <{name}> to be torch.int64, got {value.dtype}")
+
+    def _validate_triangles_shape(self, name, value):
+        if value.shape[-1] != 3:
+            raise ValueError(f"Expected the last dimension of <{name}> to be of size 3, got {value.shape[-1]}.")
+
+    def _validate_tetrahedra_shape(self, name, value):
+        if value.shape[-1] != 4:
+            raise ValueError(f"Expected the last dimension of <{name}> to be of size 4, got {value.shape[-1]}.")
---
+++ tests/mesh/test_elements.py
@@ -0,0 +1,103 @@
+import unittest
+import torch
+import sys
+
+sys.path.append("src")
+from mesh.elements import Elements
+
+
+class TestElements(unittest.TestCase):
+    def tests_if_type_error_is_raised_when_triangles_is_initialized_to_list(self):
+        with self.assertRaises(TypeError):
+            Elements(triangles=[1, 2, 3])
+
+    def tests_if_type_error_is_raised_when_triangles_is_set_to_list(self):
+        with self.assertRaises(TypeError):
+            tri = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
+            elements = Elements(triangles=tri)
+            elements.triangles = [1, 2, 3]
+
+    def tests_if_value_error_is_raised_when_triangles_is_initialized_to_1d_tensor(self):
+        with self.assertRaises(ValueError):
+            tri = torch.arange(12).to(dtype=torch.int64)
+            Elements(triangles=tri)
+
+    def tests_if_value_error_is_raised_when_triangles_is_set_to_1d_tensor(self):
+        with self.assertRaises(ValueError):
+            tri = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
+            elements = Elements(triangles=tri)
+            elements.triangles = torch.arange(12).to(dtype=torch.int64)
+
+    def tests_if_value_error_is_raised_when_triangles_is_initialized_to_3d_tensor(self):
+        with self.assertRaises(ValueError):
+            tri = torch.arange(12).reshape(2, 2, 3).to(dtype=torch.int64)
+            Elements(triangles=tri)
+
+    def tests_if_value_error_is_raised_when_triangles_is_set_to_3d_tensor(self):
+        with self.assertRaises(ValueError):
+            tri = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
+            elements = Elements(triangles=tri)
+            elements.triangles = tri.reshape(2, 2, 3).to(dtype=torch.int64)
+
+    def tests_if_value_error_is_raised_when_triangles_last_dimension_is_initialized_to_2(self):
+        with self.assertRaises(ValueError):
+            tri = torch.arange(12).reshape(6, 2).to(dtype=torch.int64)
+            Elements(triangles=tri)
+
+    def tests_if_value_error_is_raised_when_triangles_last_dimension_is_set_to_2(self):
+        with self.assertRaises(ValueError):
+            tri = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
+            elements = Elements(triangles=tri)
+            elements.triangles = torch.arange(12).reshape(6, 2).to(dtype=torch.int64)
+
+    def tests_if_value_error_is_raised_when_tetrahedras_last_dimension_is_initialized_to_3(self):
+        with self.assertRaises(ValueError):
+            tet = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
+            Elements(tetrahedra=tet)
+
+    def tests_if_value_error_is_raised_when_tetrahedras_last_dimension_is_set_to_3(self):
+        with self.assertRaises(ValueError):
+            tri = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
+            elements = Elements(triangles=tri)
+            elements.tetrahedra = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
+
+    def tests_if_tetrahedra_is_none_when_initialized(self):
+        tri = torch.arange(12).reshape(4, 3)
+        elements = Elements(triangles=tri)
+        self.assertIsNone(elements.tetrahedra)
+
+    def tests_if_value_error_is_raised_when_triangles_dtype_is_initialzied_to_torch_float32(self):
+        with self.assertRaises(ValueError):
+            tri = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
+            elements = Elements(triangles=tri)
+
+    def tests_if_value_error_is_raised_when_position_dtype_is_set_to_torch_float32(self):
+        with self.assertRaises(ValueError):
+            tri = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
+            elements = Elements(triangles=tri)
+            elements.triangles = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
+
+    def tests_if_value_error_is_raised_when_tetrahedra_dtype_is_initialzied_to_torch_float32(self):
+        with self.assertRaises(ValueError):
+            tet = torch.arange(12).reshape(3, 4).to(dtype=torch.float32)
+            elements = Elements(tetrahedra=tet)
+
+    def tests_if_value_error_is_raised_when_position_dtype_is_set_to_torch_float32(self):
+        with self.assertRaises(ValueError):
+            tet = torch.arange(12).reshape(3, 4).to(dtype=torch.int64)
+            elements = Elements(tetrahedra=tet)
+            elements.triangles = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
+
+    def tests_the_num_of_triangles(self):
+        tri = torch.arange(21).reshape(7, 3).to(dtype=torch.int64)
+        elements = Elements(triangles=tri)
+        self.assertEqual(elements.num_triangles, 7)
+
+    def tests_the_num_of_tetrahedra(self):
+        tet = torch.arange(24).reshape(6, 4).to(dtype=torch.int64)
+        elements = Elements(tetrahedra=tet)
+        self.assertEqual(elements.num_tetrahedra, 6)
+
+
+if __name__ == "__main__":
+    unittest.main()
