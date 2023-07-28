128f1d7e96e6ea4ebd13346fc030113d50ed61bb
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-07-28 13:35:08 +0200

    added validator to mesh and its test

--- src/mesh/mesh.py
+++ src/mesh/mesh.py
@@ -7,3 +7,10 @@ from mesh.elements import Elements
 class Mesh:
     nodes: Nodes = field()
     elements: Elements = field()
+
+    @elements.validator
+    def _check_max_index(self, attribute, value):
+        for element, name in zip([value.triangles, value.tetrahedra], ["triangles", "tetrahedra"]):
+            if element is not None:
+                if (element.amax() >= len(self.nodes)):
+                    raise IndexError(f"Some indices of {name} larger than the number of nodes.")
