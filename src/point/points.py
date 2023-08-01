6c8b64252c75c3822e177a5586e68f8ae4c33ed1
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-01 17:09:39 +0200

    changed nodes and holes to share their code with points.

---
+++ src/point/points.py
@@ -0,0 +1,41 @@
+import torch
+from attrs import define, field
+
+
+@define
+class Points:
+    position: torch.Tensor = field()
+    velocity: torch.Tensor = field(init=False)
+    force: torch.Tensor = field(init=False)
+
+    def __len__(self):
+        return self.position.shape[0]
+    
+    @velocity.default
+    @force.default
+    def _set_zero_by_default(self):
+        return torch.zeros_like(self.position)
+
+    @position.validator
+    def _validate_position(self, attribute, value):
+        self._validate_common_traits_of_attributes(name=attribute.name, value=value)
+
+    @velocity.validator
+    @force.validator
+    def _validate_velocity(self, attribute, value):
+        self._validate_common_traits_of_attributes(name=attribute.name, value=value)
+        self._validate_equal_shape_with_position(name=attribute.name, value=value)
+
+    def _validate_common_traits_of_attributes(self, name, value):
+        if not isinstance(value, torch.Tensor):
+            raise TypeError(f"Expected <{name}> to be of type torch.Tensor, got {type(value)}.")
+        if len(value.shape) != 2:
+            raise ValueError(f"Expected <{name}> to be a 2D tensor, got {len(value.shape)}D tensor.")
+        if value.shape[-1] != 3:
+            raise ValueError(f"Expected the last dimension of <{name}> to be of size 3, got {value.shape[-1]}.")
+        if value.dtype != torch.float32:
+            raise ValueError(f"Expected dtype of <{name}> to be torch.float32, got {value.dtype}.")
+
+    def _validate_equal_shape_with_position(self, name, value):
+        if value.shape != self.position.shape:
+            raise ValueError(f"Expected <{name}> to have the same shape as <position>, got {value.shape})")
--- tests/test_points.py
+++ tests/test_points.py
@@ -3,89 +3,89 @@ import torch
 import sys
 
 sys.path.append("src")
-from mesh.nodes import Nodes
+from point.points import Points
 
 
-class TestNodes(unittest.TestCase):
+class TestPoints(unittest.TestCase):
     def tests_if_type_error_is_raised_when_position_is_initialized_to_list(self):
         with self.assertRaises(TypeError):
-            Nodes(position=[1, 2, 3])
+            Points(position=[1, 2, 3])
 
     def tests_if_type_error_is_raised_when_position_is_set_to_list(self):
-        nodes = Nodes(position=torch.zeros([10, 3], dtype=torch.float32))
+        points = Points(position=torch.zeros([10, 3], dtype=torch.float32))
         with self.assertRaises(TypeError):
-            nodes.position = [1, 2, 3]
+            points.position = [1, 2, 3]
 
     def tests_if_value_error_is_raised_when_position_is_initialized_to_1d_tensor(self):
         with self.assertRaises(ValueError):
             p = torch.arange(12).to(dtype=torch.float32)
-            Nodes(position=p)
+            Points(position=p)
 
     def tests_if_value_error_is_raised_when_position_is_set_to_1d_tensor(self):
         p = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
-        nodes = Nodes(position=p)
+        points = Points(position=p)
         with self.assertRaises(ValueError):
-            nodes.position = torch.arange(12).to(dtype=torch.float32)
+            points.position = torch.arange(12).to(dtype=torch.float32)
 
     def tests_if_value_error_is_raised_when_position_is_initialized_to_3d_tensor(self):
         p = torch.arange(12).reshape(2, 2, 3).to(dtype=torch.float32)
         with self.assertRaises(ValueError):
-            Nodes(position=p)
+            Points(position=p)
 
     def tests_if_value_error_is_raised_when_position_is_set_to_3d_tensor(self):
         p = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
-        nodes = Nodes(position=p)
+        points = Points(position=p)
         with self.assertRaises(ValueError):
-            nodes.position = p.reshape(2, 2, 3).to(dtype=torch.float32)
+            points.position = p.reshape(2, 2, 3).to(dtype=torch.float32)
 
     def tests_if_value_error_is_raised_when_positions_last_dimension_is_initialized_to_2(self):
         p = torch.arange(12).reshape(6, 2).to(dtype=torch.float32)
         with self.assertRaises(ValueError):
-            Nodes(position=p)
+            Points(position=p)
 
     def tests_if_value_error_is_raised_when_positions_last_dimension_is_set_to_2(self):
         p = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
-        nodes = Nodes(position=p)
+        points = Points(position=p)
         with self.assertRaises(ValueError):
-            nodes.position = torch.arange(12).reshape(6, 2).to(dtype=torch.float32)
+            points.position = torch.arange(12).reshape(6, 2).to(dtype=torch.float32)
 
     def tests_if_value_error_is_raised_when_position_dtype_is_initialzied_to_torch_int64(self):
         p = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
         with self.assertRaises(ValueError):
-            Nodes(position=p)
+            Points(position=p)
 
     def tests_if_value_error_is_raised_when_position_dtype_is_set_to_torch_int64(self):
         p = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
-        nodes = Nodes(position=p)
+        points = Points(position=p)
         with self.assertRaises(ValueError):
-            nodes.position = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
+            points.position = torch.arange(12).reshape(4, 3).to(dtype=torch.int64)
 
-    def tests_if_velocity_is_none_when_uninitialized(self):
+    def tests_if_velocity_is_zeros_like_position_by_default(self):
         p = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
-        nodes = Nodes(position=p)
-        self.assertIsNone(nodes.velocity)
+        points = Points(position=p)
+        self.assertTrue(torch.equal(points.velocity, torch.zeros_like(points.position)))
 
     def tests_if_value_error_is_raised_when_velocity_is_set_to_tensor_of_a_different_shape_to_position(self):
         p = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
-        nodes = Nodes(position=p)
+        points = Points(position=p)
         with self.assertRaises(ValueError):
-            nodes.velocity = torch.arange(24).reshape(8, 3).to(dtype=torch.float32)
+            points.velocity = torch.arange(24).reshape(8, 3).to(dtype=torch.float32)
 
-    def tests_if_force_is_none_when_uninitialized(self):
+    def tests_if_force_is_zeros_like_position_by_default(self):
         p = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
-        nodes = Nodes(position=p)
-        self.assertIsNone(nodes.force)
+        points = Points(position=p)
+        self.assertTrue(torch.equal(points.force, torch.zeros_like(points.position)))
 
     def tests_if_value_error_is_raised_when_force_is_set_to_tensor_of_a_different_shape_to_position(self):
         p = torch.arange(12).reshape(4, 3).to(dtype=torch.float32)
-        nodes = Nodes(position=p)
+        points = Points(position=p)
         with self.assertRaises(ValueError):
-            nodes.force = torch.arange(24).reshape(8, 3).to(dtype=torch.float32)
+            points.force = torch.arange(24).reshape(8, 3).to(dtype=torch.float32)
 
-    def tests_the_length_of_nodes(self):
+    def tests_the_length_of_points(self):
         p = torch.arange(21).reshape(7, 3).to(dtype=torch.float32)
-        nodes = Nodes(position=p)
-        self.assertEqual(len(nodes), 7)
+        points = Points(position=p)
+        self.assertEqual(len(points), 7)
 
 
 if __name__ == "__main__":
