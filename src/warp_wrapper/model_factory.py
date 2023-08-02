ca2553213d89329ddb95c0d13d95c946fb3bd52b
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-02 17:17:09 +0200

    added model_factory and its test.

---
+++ src/warp_wrapper/model_factory.py
@@ -0,0 +1,78 @@
+import torch
+import numpy as np
+import warp as wp
+from warp.sim import Model, ModelBuilder
+import sys
+
+sys.path.append("src")
+from mesh.mesh import Mesh
+
+wp.init()
+
+
+class ModelFactory:
+    def __init__(self, soft_mesh: Mesh = None, shape_mesh: Mesh = None, device: str = "cuda"):
+        self._soft_mesh = soft_mesh
+        self._shape_mesh = shape_mesh
+        self._device = device
+
+    def create(self) -> Model:
+        builder: ModelBuilder = wp.sim.ModelBuilder()
+        self._add_soft_mesh(builder) if self._soft_mesh is not None else None
+        self._add_shape_mesh(builder) if self._shape_mesh is not None else None
+        model = builder.finalize(self._device)
+        model = self._update_model_attributes(model)
+        return model
+
+    def _add_soft_mesh(self, builder: ModelBuilder):
+        properties = self._soft_mesh.properties
+        k_lambda, k_mu = wp.utils.lame_parameters(properties.youngs_modulus, properties.poissons_ratio)
+        builder.add_soft_mesh(
+            pos=np.array([0, 0, 0]),
+            rot=np.array([0, 0, 0, 1]),
+            scale=1,
+            vel=[0, 0, 0],
+            vertices=self._soft_mesh.nodes.position.cpu().numpy(),
+            indices=self._soft_mesh.elements.tetrahedra.cpu().numpy().reshape(-1),
+            density=properties.density,
+            k_mu=k_mu,
+            k_lambda=k_lambda,
+            k_damp=properties.damping_factor,
+        )
+        self._apply_boundary_conditions(builder)
+        self._set_soft_mesh_triangles(builder) # TODO: see if this is different from what mesh_factory creates
+
+    def _apply_boundary_conditions(self, builder: ModelBuilder):
+        builder.particle_mass = torch.tensor(builder.particle_mass, device=self._device)
+        mask = self._is_inside_frozen_bounding_box(self._soft_mesh)
+        builder.particle_mass[mask] = 0.0
+        builder.particle_mass = builder.particle_mass.tolist()
+
+    @staticmethod
+    def _is_inside_frozen_bounding_box(soft_mesh: Mesh):
+        frozen_bounding_box = soft_mesh.properties.frozen_bounding_box
+        points = soft_mesh.nodes.position
+        bounding_box_min, bounding_box_max = torch.tensor(frozen_bounding_box, device=points.device).reshape(2, 3)
+        return torch.all((bounding_box_min <= points) & (points <= bounding_box_max), dim=1)
+
+    def _set_soft_mesh_triangles(self, builder: ModelBuilder):
+        self._soft_mesh.elements.triangles = torch.tensor(builder.tri_indices, device=self._device, dtype=torch.int32)
+
+    def _add_shape_mesh(self, builder: ModelBuilder):
+        builder.add_shape_mesh(
+            body=-1,
+            mesh=wp.sim.Mesh(
+                self._shape_mesh.nodes.position.cpu().numpy(),
+                self._shape_mesh.elements.triangles.cpu().numpy().reshape(-1),
+            ),
+            density=self._shape_mesh.properties.density,
+        )
+
+    @staticmethod
+    def _update_model_attributes(model: Model):
+        model.tri_ke, model.tri_ka, model.tri_kd, model.tri_kb = 0.0, 0.0, 0.0, 0.0
+        model.ground = False
+        model.gravity = (0.0, -9.8, 0.0)
+        model.soft_contact_distance = 0.0
+        model.soft_contact_ke, model.soft_contact_kd, model.soft_contact_kf = 0.1, 0.1, 0.1
+        return model
---
+++ tests/test_model_factory.py
@@ -0,0 +1,34 @@
+import unittest
+import sys
+from pathlib import Path
+import numpy as np
+sys.path.append("src")
+
+from warp_wrapper.model_factory import ModelFactory
+from mesh.scad import Scad
+from mesh.mesh_factory import MeshFactoryFromScad
+from mesh.mesh_properties import MeshProperties
+from warp.sim import Model
+
+class TestModelFactory(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        file = Path("tests/data/caterpillar.scad")
+        parameters = Path("tests/data/caterpillar_scad_params.json")
+        scad = Scad(file, parameters)
+        mesh = MeshFactoryFromScad(scad).create()
+        mesh.properties = MeshProperties(name="caterpillar",
+                                         density=1080.0,
+                                         youngs_modulus=149_000,
+                                         poissons_ratio=0.45,
+                                         damping_factor=0.4,
+                                         frozen_bounding_box=[-np.inf, -np.inf, 0, np.inf, np.inf, 2])
+        cls.model_factory = ModelFactory(soft_mesh=mesh, device="cuda")
+
+    def tests_if_model_factory_creates_a_model_with_a_soft_mesh(self):
+        model = self.model_factory.create()
+        self.assertTrue(isinstance(model, Model))
+
+if __name__ == "__main__":
+    unittest.main()
