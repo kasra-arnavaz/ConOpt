586ba640de3c6ea425c799c7cbf75c7ada362eb8
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-02 13:44:56 +0200

    added nodes_force

---
+++ src/mesh/nodes_force.py
@@ -0,0 +1,16 @@
+from mesh.mesh import Nodes
+import sys
+sys.path.append("src")
+
+from cable.holes import Holes
+from cable.barycentric import Barycentric
+
+
+class NodesForce:
+    def __init__(self, nodes: Nodes, holes: Holes, barycentric: Barycentric):
+        self._nodes = nodes
+        self._holes = holes
+        self._barycentric = barycentric
+
+    def update(self):
+        self._nodes.force = self._barycentric.NxH @ self._holes.force
