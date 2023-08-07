498f4bb3ce7a9534912eda1c6e4cbfe59e5dfa61
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-07 16:18:14 +0200

    removed states from attributes

--- src/mesh/nodes_position_and_velocity.py
+++ src/mesh/nodes_position_and_velocity.py
@@ -6,22 +6,24 @@ sys.path.append("src")
 from warp_wrapper.update_state import UpdateState
 from warp.sim import Model
 
+import warp as wp
+
 
 class NodesPositionAndVelocity:
     def __init__(self, nodes: Nodes, model: Model, dt: float):
         self._nodes = nodes
         self._model = model
         self._dt = dt
-        self._state_now = self._model.state(requires_grad=True)
-        self._state_next = self._model.state(requires_grad=True)
 
     def update(self):
+        state_now = self._model.state(requires_grad=True)
+        state_next = self._model.state(requires_grad=True)
         self._nodes.position, self._nodes.velocity = UpdateState.apply(
             self._nodes.force,
             self._nodes.position,
             self._nodes.velocity,
             self._model,
             self._dt,
-            self._state_now,
-            self._state_next,
+            state_now,
+            state_next,
         )
