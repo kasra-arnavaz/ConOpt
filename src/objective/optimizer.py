17aae76992ec9d1c76d2b51dea7812b1bf0bb3f5
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-10 16:06:44 +0200

    changed optimizer to set new variable parameters.

--- src/objective/optimizer.py
+++ src/objective/optimizer.py
@@ -22,7 +22,9 @@ class GradientDescent(Optimizer):
 
     @torch.no_grad()
     def step(self):
-        self._variables.parameters =  self._variables.parameters - self._variables.gradients * self._learning_rate
+        self._variables.set_gradients()
+        for i in range(len(self._variables)):
+            self._variables.parameters[i] =  self._variables.parameters[i] - self._variables.gradients[i] * self._learning_rate
 
 
 class Adam(Optimizer):
@@ -32,15 +34,17 @@ class Adam(Optimizer):
         self._learning_rate = learning_rate
         self._betas = [0.9, 0.99]
         self._eps = 1e-07
-        self._m = torch.zeros_like(variables.parameters)
-        self._v = torch.zeros_like(variables.parameters)
         self._t = 0
 
     @torch.no_grad()
     def step(self):
-        self._m = self._betas[0] * self._m + (1.0 - self._betas[0]) * self._variables.gradients
-        self._v = self._betas[1] * self._v + (1.0 - self._betas[1]) * self._variables.gradients * self._variables.gradients
-        mhat = self._m / (1.0 - (self._betas[0] ** (self._t + 1.0)))
-        vhat = self._v / (1.0 - (self._betas[1] ** (self._t + 1.0)))
-        self._variables.parameters = self._variables.parameters - self._learning_rate * mhat / (torch.sqrt(vhat) + self._eps)
+        self._variables.set_gradients()
+        for i in range(len(self._variables)):
+            self._m = torch.zeros_like(self._variables.parameters[i])
+            self._v = torch.zeros_like(self._variables.parameters[i])
+            self._m = self._betas[0] * self._m + (1.0 - self._betas[0]) * self._variables.gradients[i]
+            self._v = self._betas[1] * self._v + (1.0 - self._betas[1]) * self._variables.gradients[i] * self._variables.gradients[i]
+            mhat = self._m / (1.0 - (self._betas[0] ** (self._t + 1.0)))
+            vhat = self._v / (1.0 - (self._betas[1] ** (self._t + 1.0)))
+            self._variables.parameters[i] = self._variables.parameters[i] - self._learning_rate * mhat / (torch.sqrt(vhat) + self._eps)
         self._t += 1
