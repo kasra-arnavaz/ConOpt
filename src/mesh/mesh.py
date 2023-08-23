4603f22aad5d0a4b680b13ab21036383cbad62d0
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-23 16:50:47 +0200

    added cables as attribute

--- src/mesh/mesh.py
+++ src/mesh/mesh.py
@@ -1,6 +1,8 @@
 from attrs import define, field
+from typing import List
 from mesh.nodes import Nodes
 from mesh.elements import Elements
+from cable.cable import Cable
 from mesh.mesh_properties import MeshProperties
 
 
@@ -8,11 +10,14 @@ from mesh.mesh_properties import MeshProperties
 class Mesh:
     nodes: Nodes = field()
     elements: Elements = field()
+    cables: List[Cable] = field(default=None)
     properties: MeshProperties = field(default=None)
 
     @elements.validator
     def _check_max_index(self, attribute, value):
-        for element, name in zip([value.triangles, value.tetrahedra], ["triangles", "tetrahedra"]):
+        elements = [value.triangles, value.tetrahedra]
+        names = ["triangles", "tetrahedra"]
+        for element, name in zip(elements, names):
             if element is not None:
                 if element.amax() >= len(self.nodes):
                     raise IndexError(f"Some indices of {name} larger than the number of nodes.")
