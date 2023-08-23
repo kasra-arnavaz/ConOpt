37a9729bf8365a5871256afa0a35198d48009651
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-23 16:55:32 +0200

    simulation tested

--- tests/integration/test_simulation.py
+++ tests/integration/test_simulation.py
@@ -7,12 +7,13 @@ from pathlib import Path
 from simulation.simulation import Simulation
 from mesh.mesh_factory import MeshFactoryFromScad, MeshFactoryFromObj
 from mesh.scad import Scad
-from cable.cable_factory import CableFactory
-from cable.holes_factory import HolesFactoryFromListOfPositions
+from cable.cable_factory import MultiCableFactory
+from cable.holes_factory import MultiHolesFactory
 from cable.holes_initial_position import HolesInitialPosition
 from point.transform import Transform, get_quaternion
 from mesh.mesh_properties import MeshProperties
 from simulation.simulation_properties import SimulationProperties
+from simulation.scene import Scene
 
 
 class TestSimulationWithCheckpoint(unittest.TestCase):
@@ -39,7 +40,7 @@ class TestSimulationWithCheckpoint(unittest.TestCase):
             rotation=get_quaternion(vector=[1, 0, 0], angle_in_degrees=90), scale=[0.001, 0.001, 0.001]
         )
         holes_position = HolesInitialPosition(scad).get()
-        holes = HolesFactoryFromListOfPositions(holes_position).create()
+        holes = MultiHolesFactory(holes_position).create()
         transform.apply(object_mesh.nodes)
         transform.apply(cls.gripper_mesh.nodes)
         for hole in holes:
@@ -50,29 +51,27 @@ class TestSimulationWithCheckpoint(unittest.TestCase):
             torch.tensor(0.0, device="cuda", requires_grad=True),
         ]
 
-        cables = CableFactory(stiffness=100, damping=0.01, pull_ratio=cls.pull_ratio).create()
+        cables = MultiCableFactory(holes=holes, pull_ratio=cls.pull_ratio, stiffness=100, damping=0.01).create()
+        cls.gripper_mesh.cables = cables
         sim_properties = SimulationProperties(
             duration=0.02, segment_duration=0.01, dt=2.1701388888888886e-05, device="cuda"
         )
-        cls.simulation = Simulation(
-            gripper_mesh=cls.gripper_mesh,
-            object_mesh=object_mesh,
-            cables=cables,
-            holes=holes,
-            properties=sim_properties,
-        )
+        scene = Scene(gripper=cls.gripper_mesh, object=object_mesh, device="cuda")
+        cls.simulation = Simulation(scene=scene, properties=sim_properties)
         cls.gripper_mesh.nodes.position, cls.gripper_mesh.nodes.velocity = cls.simulation(
             cls.gripper_mesh.nodes.position, cls.gripper_mesh.nodes.velocity
         )
 
     def tests_if_simulation_releases_memory_after_each_segment(self):
         memory = self.simulation.free_memory
+        print(memory)
         self.assertTrue(max(memory) - min(memory) <= 0.1)
 
     def tests_if_a_gradients_of_pull_ratio_are_not_none_given_a_loss(self):
         loss = self.gripper_mesh.nodes.position.sum()
         loss.backward()
         for pull in self.pull_ratio:
+            print(pull.grad)
             self.assertIsNotNone(pull.grad)
 
 
