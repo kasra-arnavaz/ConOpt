016d8b04c33bfea867e4d666d085eef1fa7e0948
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-23 16:53:13 +0200

    added num_segments and num_steps_per_segment

--- src/simulation/simulation_properties.py
+++ src/simulation/simulation_properties.py
@@ -6,7 +6,17 @@ class SimulationProperties:
     duration: float = field(metadata={"unit": "seconds"})
     segment_duration: float = field(metadata={"unit": "seconds"})
     dt: float = field(metadata={"unit": "seconds"})
+    num_segments: int = field()
+    num_steps_per_segment: int = field()
     device: str = field(default="cuda")
+
+    @num_segments.default
+    def _calc_num_segments(self):
+        return int(self.duration / self.segment_duration)
+
+    @num_steps_per_segment.default
+    def _calc_num_steps_per_segment(self):
+        return int(self.segment_duration / self.dt)
 
     @segment_duration.validator
     def _check_compatibility_of_duration(self, attribute, value):
