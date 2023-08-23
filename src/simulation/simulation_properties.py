3f25accb339e3192a78852a13aada60c7161e456
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-23 13:55:59 +0200

    added validator for segment_duration

--- src/simulation/simulation_properties.py
+++ src/simulation/simulation_properties.py
@@ -7,3 +7,11 @@ class SimulationProperties:
     segment_duration: float = field(metadata={"unit": "seconds"})
     dt: float = field(metadata={"unit": "seconds"})
     device: str = field(default="cuda")
+
+    @segment_duration.validator
+    def _check_compatibility_of_duration(self, attribute, value):
+        if self._segment_duration_not_a_multiplier_of_duration():
+            raise ValueError(f"Expected <{attribute.name}> to be a multiplier of <duration>.")
+
+    def _segment_duration_not_a_multiplier_of_duration(self):
+        return self.duration % self.segment_duration != 0
