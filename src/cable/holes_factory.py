60cd52fbca9ddb58918f9ee12b87afe60119626e
kasra-arnavaz <arnavaz.kasra@gmail.com>
2023-08-23 16:49:23 +0200

    added MultHolesFactory

--- src/cable/holes_factory.py
+++ src/cable/holes_factory.py
@@ -1,19 +1,12 @@
 import torch
 from typing import List
-from abc import ABC, abstractmethod
 from cable.holes import Holes
 
 
-class HolesFactory(ABC):
-    @abstractmethod
-    def create(self) -> List[Holes]:
-        pass
-
-
-class HolesFactoryFromListOfPositions(HolesFactory):
+class MultiHolesFactory:
     def __init__(self, list_of_positions: List[torch.Tensor], device: str = "cuda"):
         self._list_of_positions = list_of_positions
         self._device = device
 
-    def create(self):
-        return [Holes(position.to(device=self._device).requires_grad_()) for position in self._list_of_positions]
+    def create(self) -> List[Holes]:
+        return [Holes(position.to(device=self._device)) for position in self._list_of_positions]
