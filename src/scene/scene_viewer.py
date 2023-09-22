b610153ab982af3a90365dac0eca811b6dee9e3e
Kasra <arnavaz.kasra@gmail.com>
2023-09-22 17:20:47 +0200

    added speed factor

--- src/scene/scene_viewer.py
+++ src/scene/scene_viewer.py
@@ -11,15 +11,16 @@ wp.init()
 
 
 class SceneViewer:
-    def __init__(self, scene: Scene, path: str):
+    def __init__(self, scene: Scene, path: str, speed_factor: float = 1.0):
         self._scene = scene
         os.makedirs(path, exist_ok=True)
         stage = Usd.Stage.CreateNew(f"{path}/scene.usd")
         self.usd = wp.render.UsdRenderer(stage)
+        self._speed_factor = speed_factor
 
-    def record(self, time: float = 0.0):
+    def record(self, time: float):
         with wp.ScopedTimer("render", print=False):
-            self.usd.begin_frame(time)
+            self.usd.begin_frame(time/self._speed_factor)
             for mesh in self._scene.all_meshes():
                 self.usd.render_mesh(
                     name=mesh.properties.name,
